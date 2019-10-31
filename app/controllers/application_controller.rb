e7c164fa8bf5fe3483088b7229ba5f5785d0d6cf
daniel <dhrc1997@gmail.com>
2019-10-31 17:33:09 -0300

    some fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
-class ApplicationController < ActionController::API
-  skip_before_action :verify_authenticity_token
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -22,4 +22,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
+
+  allow do
+    origins 'https://trocaaqui.appspot.com'
+    resource '*',
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 end
