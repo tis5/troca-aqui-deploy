0076583885525e6f79fd497c2263779e78a4b63e
mr.danielcosta <31771918+danielhrc@users.noreply.github.com>
2019-11-22 00:49:34 -0300

    restore schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,5 +14,37 @@ ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  
+  create_table "pessoas", force: :cascade do |t|
+    t.string "nome"
+    t.string "email"
+    t.string "cidade"
+    t.datetime "data_nasc"
+    t.string "telefone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+  
+  create_table "items", force: :cascade do |t|
+    t.string "nome"
+    t.string "categoria"
+    t.float "valor_aprox"
+    t.string "desejo"
+    t.integer "quant"
+    t.bigint "pessoa_id", null: false
+    t.boolean "disp"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pessoa_id"], name: "index_items_on_pessoa_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "items", "pessoas"
 
 end
